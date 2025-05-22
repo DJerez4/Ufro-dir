a994d74e4814ed09a1da4370ff63db7d570bbe6f
DJerez4 <d.jerez04@ufromail.cl>
2025-05-22 09:02:31 -0400

    Initial commit

---
+++ src/main/java/Isoft/Ufro_dir/UfroDirApplication.java
@@ -0,0 +1,13 @@
+package Isoft.Ufro_dir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfroDirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfroDirApplication.class, args);
+	}
+
+}
